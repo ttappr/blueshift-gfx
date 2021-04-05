921d4a9feaa083ab7b32dd2161de40dc23b60cfb
ttappr <tmtappr@gmail.com>
2021-04-04 17:14:21 -0700

    Cleaned up Program.link().

--- src/program.rs
+++ src/program.rs
@@ -119,20 +119,22 @@ impl Program {
         // Create the program.
         self.pid = ctx.create_program();
         
+        let ref pid = self.pid.as_ref().unwrap().clone();
+        
         // Attach the shaders.
-        ctx.attach_shader(self.pid(), self.vertex_shader.sid());
-        ctx.attach_shader(self.pid(), self.fragment_shader.sid());
+        ctx.attach_shader(pid, self.vertex_shader.sid());
+        ctx.attach_shader(pid, self.fragment_shader.sid());
         
         // Invoke the binding done callback.
         if let Some(callback) = &self.bind_attr_callback {
             callback();
         }        
         // Link the program.
-        ctx.link_program(self.pid());
+        ctx.link_program(pid);
         
         // If debug is true, print out any diagnostic info that just happened.
         if debug {
-            if let Some(log) = ctx.get_program_info_log(self.pid()) {
+            if let Some(log) = ctx.get_program_info_log(pid) {
                 if !log.is_empty() {
                     let msg = format!("[ {} ]\n", self.name);
                     console::log_2(&msg.into(), &log.into());
@@ -140,27 +142,26 @@ impl Program {
             }
         }
         // Check the link status and exit with 'false' if there was a failure.
-        let status = ctx.get_program_parameter(self.pid(), Ctx::LINK_STATUS);
+        let status = ctx.get_program_parameter(pid, Ctx::LINK_STATUS);
         if status.is_falsy() {
             self.delete_id();
             return false;
         }
         
         // Get the number of attributes and add them to the attribute array.
-        let nattr = ctx.get_program_parameter(self.pid(), 
-                                              Ctx::ACTIVE_ATTRIBUTES);
+        let nattr = ctx.get_program_parameter(pid, Ctx::ACTIVE_ATTRIBUTES);
         let nattr = nattr.as_f64().unwrap() as u32;
         
         for i in 0..nattr {
-            let attrib = ctx.get_active_attrib(self.pid(), i).unwrap();
+            let attrib = ctx.get_active_attrib(pid, i).unwrap();
             self.add_vertex_attr(attrib.name(), attrib.type_());
         }
         // Get the number of uniforms and add them to the uniform array.
-        let nuni = ctx.get_program_parameter(self.pid(), Ctx::ACTIVE_UNIFORMS);
+        let nuni = ctx.get_program_parameter(pid, Ctx::ACTIVE_UNIFORMS);
         let nuni = nuni.as_f64().unwrap() as u32;
         
         for i in 0..nuni {
-            let uni = ctx.get_active_uniform(self.pid(), i).unwrap();
+            let uni = ctx.get_active_uniform(pid, i).unwrap();
             self.add_uniform(uni.name(), uni.type_());
         }
 
